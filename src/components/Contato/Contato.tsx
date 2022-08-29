a8e91a0f3ecd37648a569a18e098203055e3c7bc
João Vitor <vitorjoao39207@gmail.com>
2022-08-29 20:43:04 -0300

    Fazendo a funcionalidade de envio do formulario

--- src/components/Contato/Contato.tsx
+++ src/components/Contato/Contato.tsx
@@ -9,9 +9,7 @@ export function Contato() {
                         <h1>Enviar Mensagem</h1>
                         <span>Tá afim de me mandar uma mensagem por email? Escreve ai!</span>
                     </div>
-                    <form name="contact" method="POST" data-netlify="true">
-                        <input type="hidden" name="form-name" value="name_of_my_form" />
-
+                    <form action="https://formsubmit.co/your@email.com" method="POST">
                         <div className="inputs">
                             <span>Nome</span>
                             <input type="text" name="nome" placeholder="Insira aqui o seu Nome..." />
