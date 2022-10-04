44bd787e6c404b9909485b75acbdaa1716a26ed2
João Vitor <vitorjoao39207@gmail.com>
2022-10-04 02:43:14 -0300

    Arrumando a resposividade do titulo do carousel

--- src/components/Projetos/ProjetosStyle.tsx
+++ src/components/Projetos/ProjetosStyle.tsx
@@ -8,26 +8,44 @@ export const ProjetosCss = styled.div`
     align-items: center;
     padding: 10px;
 
-    ::before{
-        content: '';
-        display: block;
-        width: 1024px;
-        height: 2px;
-        background-color: rgba(0, 0, 0, 0.4);
-        margin-bottom: 50px;
-    }
-    h1{
-        color: var(--black);
-        font-size: 38px;
-    }
-    span{
-        color: #888;
-        font-weight: 500;
-    }
+::before{
+    content: '';
+    display: block;
+    width: 1024px;
+    height: 2px;
+    background-color: rgba(0, 0, 0, 0.4);
+    margin-bottom: 50px;
+}
+h1{
+    color: var(--black);
+    font-size: 38px;
+}
+span{
+    color: #888;
+    font-weight: 500;
+}
+.tituloCarousel{
+    width: 900px;
+}
+.tituloCarousel h2{
+    font-size: 25px;
+}
+
+@media (max-width: 1030px){
     .tituloCarousel{
-        width: 1070px;
+        width: 700px;
     }
-    .tituloCarousel h2{
-        font-size: 25px;
+}
+@media (max-width: 800px){
+    .tituloCarousel{
+        width: 600px;
     }
+}
+@media (max-width: 700px){
+    .tituloCarousel{
+        width: 100px;
+        margin-top: 50px;
+    }
+
+}
 `;
